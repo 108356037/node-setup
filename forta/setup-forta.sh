a2f17d6f182fc07cbee7243470751bed99d88eb4
蔡維哲 <108356037@nccu.edu.tw>
2022-06-16 20:29:37 +0800

    update /etc/docker/daemon.json

--- forta/setup-forta.sh
+++ forta/setup-forta.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+sudo bash -c 'cat <<EOF > /etc/docker/daemon.json 
+{
+   "default-address-pools": [
+        {
+            "base":"172.17.0.0/12",
+            "size":16
+        },
+        {
+            "base":"192.168.0.0/16",
+            "size":20
+        },
+        {
+            "base":"10.99.0.0/16",
+            "size":24
+        }
+    ]
+}
+EOF'
+
+sudo systemctl restart docker
+
 forta init --passphrase $FORTA_PASSPHRASE
 
 sudo mkdir -p /etc/systemd/system/forta.service.d
